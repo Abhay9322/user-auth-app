f7127e46556336b059f767d859ea1d1c6b2c5bf4
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-19 09:17:15 +0530

    Modified middleware logic

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,11 +2,14 @@ import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
 export const protect = async (req,res,next) => {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!token) return res.status(401).json({ message: 'No token, not authorized'});
+    if (!authHeader || !authHeader.startWith('Bearer')) {
+        return res.status(401).json({ message: 'No token, not authorized '});
+    }
 
     try {
+        const token = authHeader.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.userId).select('-password');
         next();
