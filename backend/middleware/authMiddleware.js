10d1a4d5cef45423b35b761bedea57fa1c451f2c
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-19 08:20:44 +0530

    feat: add JWT auth middleware to protect private routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+
+export const protect = async (req,res,next) => {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'No token, not authorized'});
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.userId).select('-password');
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token '});
+    }
+};
